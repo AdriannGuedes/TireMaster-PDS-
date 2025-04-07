7c2054d298bb7597daf06ea31d8560c494b3ffe2
Adriann Guedes <adrianguedes902@gmail.com>
2025-04-07 19:44:58 -0300

    Adiciona rotas da API auth e crud pneus

---
+++ backend/routes/pneusRoutes.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import {
+  listarPneus,
+  obterPorMarca,
+  obterPorMedida,
+  criarPneu,
+  atualizarPneu,
+  excluirPneu
+} from '../controllers/pneusController.js';
+
+import { verificarToken } from '../middlewares/authMiddleware.js';
+
+const router = express.Router();
+
+router.use(verificarToken);
+
+router.get('/', listarPneus);
+router.get('/marca/:marca', obterPorMarca);
+router.get('/medida/:medida', obterPorMedida);
+router.post('/cadastrarPneu', criarPneu);
+router.put('/:id', atualizarPneu);
+router.delete('/delete/:id', excluirPneu);
+
+export default router;
