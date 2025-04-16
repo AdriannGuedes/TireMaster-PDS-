4b3e8c275947830f8a5e9b93a873fc34febfd551
Adriann Guedes <adrianguedes902@gmail.com>
2025-04-16 19:45:53 -0300

    Implementando todas as rotas ao sistema e a validação do cache

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,25 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/authRoutes.js'
 import pneuRoutes from './routes/pneusRoutes.js'
+import vendasRoutes from './routes/vendasRoutes.js'
+import noCacheMiddleware from './middlewares/noCacheMiddleware.js';
+import notificacoesRoutes from './routes/notifRoutes.js';
+import relatorioRoutes from './routes/relatorioRoutes.js';
+
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+app.use(noCacheMiddleware);
+
 app.use('/login', authRoutes);
 app.use('/pneus', pneuRoutes);
+app.use('/vendas', vendasRoutes);
+app.use('/notificacoes', notificacoesRoutes);
+app.use('/relatorio', relatorioRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,() => {console.log(`Servidor Rodando na porta ${PORT}`)});
